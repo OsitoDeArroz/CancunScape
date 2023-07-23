ffb5ef4dcfb668bef4b4c40a8ce389cff4fc6a04
borisjvc <boris_25@live.com.mx>
2023-07-23 12:57:22 -0500

    El titulo de la card ahora tiene espacio definido

--- frontend/src/componentes/TarjetaTour.js
+++ frontend/src/componentes/TarjetaTour.js
@@ -5,11 +5,12 @@ import { FaInfoCircle } from "react-icons/fa";
 import "rsuite/dist/rsuite.min.css";
 
 function TarjetaTour({ imgSrc, title, description, duration, price, link }) {
-    
+
     const cardTitleStyle = {
         fontFamily: 'Pacifico, cursive',
-        fontSize: '24px',
+        fontSize: '22px',
         marginBottom: '10px',
+        minHeight: '60px'
     };
 
     const cardTextStyle = {
@@ -24,14 +25,13 @@ function TarjetaTour({ imgSrc, title, description, duration, price, link }) {
                 <Link to={link}>
                     <img src={imgSrc} className="card-img-top" alt={title} />
                 </Link>
-                <div className="card-body card-color">
+                <div className="card-body card-color" >
                     <h4 className="card-title" style={cardTitleStyle}>{title}</h4>
-                    <p className="card-text" style={cardTextStyle}>{description}</p>
-                    <p className="card-text" style={cardTextStyle}>Duración: {duration} dias</p>
-                    <h5 className="card-text" style={cardTextStyle}>MXN {price}</h5>
+                    <p className="card-text" style={cardTextStyle}>Duración: {duration} horas</p>
+                    <h5 className="card-text" style={cardTextStyle}>Precio: MXN {price}</h5>
                     <div className="mt-auto" align="center">
                         <Link to={link}>
-                            <Button color="yellow" appearance="subtle" startIcon={<FaInfoCircle/>}> Más información </Button>
+                            <Button color="yellow" appearance="subtle" startIcon={<FaInfoCircle />}> Más información </Button>
                         </Link>
                     </div>
                 </div>
