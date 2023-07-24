b864c702a15b5a75879ff88eaa40cf00368951f1
borisjvc <boris_25@live.com.mx>
2023-07-23 19:07:33 -0500

    se modifico el registro

--- frontend/src/Paginas/registro.js
+++ frontend/src/Paginas/registro.js
@@ -4,6 +4,7 @@ import Encabezado from "../componentes/Encabezado";
 import { Link } from "react-router-dom";
 
 function Registro() {
+
     return (
         <>
             <div align="center">
@@ -11,23 +12,23 @@ function Registro() {
                 <Form>
                     <Form.Group controlId="nombre">
                         <Form.ControlLabel>Nombre completo</Form.ControlLabel>
-                        <Form.Control name="nombre" />
+                        <Form.Control name="nombre" placeholder="Nombre"/>
                     </Form.Group>
                     <Form.Group controlId="contacto">
                         <Form.ControlLabel>Telefono</Form.ControlLabel>
-                        <Form.Control name="contacto" type="number" />
+                        <Form.Control name="contacto" type="number" placeholder="1234567890"/>
                     </Form.Group>
                     <Form.Group controlId="email">
                         <Form.ControlLabel>Email</Form.ControlLabel>
-                        <Form.Control name="email" type="email" />
+                        <Form.Control name="email" type="email" placeholder="email@email.com"/>
                     </Form.Group>
                     <Form.Group controlId="password">
                         <Form.ControlLabel>Contraseña</Form.ControlLabel>
-                        <Form.Control name="password" type="password" autoComplete="off" />
+                        <Form.Control name="password" type="password" autoComplete="off" placeholder="*********"/>
                     </Form.Group>
                     <Form.Group controlId="password">
                         <Form.ControlLabel>Confirmar contraseña</Form.ControlLabel>
-                        <Form.Control name="passwordConfirm" type="password" autoComplete="off" />
+                        <Form.Control name="passwordConfirm" type="password" autoComplete="off" placeholder="*********"/>
                     </Form.Group>
                     <Form.Group>
                         <Link to="/">
